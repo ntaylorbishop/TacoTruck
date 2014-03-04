54aa3f0cdbc3040915069aaeae5faa6999dc19b5
CaptainMustang8 <nmorris@smu.edu>
2014-03-04 15:07:58 -0600

    Building order confirmation page

---
+++ confirmation.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+	<title>Taco Truck -- Order Confirmation</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link href="css/styles.css" rel="stylesheet">
+	<link href="css/jquery-ui-1.10.4.custom.css" rel="stylesheet" />
+	<script src="js/jQuery.js"></script>
+	<script src="js/jquery-ui-1.10.4.custom.js"></script>
+	<script src="js/jquery-ui-1.10.4.custom.min.js"></script>
+	<script src="js/main.js"></script>
+</head>
+
+<body>
+	<div id="topBar"> <img src="img/taco_truck_logo.png" id="logo">
+    <div id="links"> <a href="#homepage">Home</a> | <a href="#account">Account</a> | <a href="#cart">Cart</a> | <a href="#logoff">Log Off</a>
+    <div id="divider"></div>
+  	</div>
+
+
+
+<footer>
+  <div style="text-align:center">Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
+</footer>
+</body>
+</html>
