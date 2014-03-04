c77bde032e33287825d8a7fc48f15232e70a3927
jkpolitz <jpolitz@smu.edu>
2014-03-03 20:34:27 -0600

    fixing main.js

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
 	resolution -= 800;
 	resolution /= 2;
 	$("#topBar").css('left',resolution);
+	$("#accordion").accordion();
+});
+
+
+$(function() {
+    $("#accordion").accordion();
 });
