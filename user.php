e82d939c17a1f994b4980c9b6ff555189f67d63c
CaptainMustang8 <nmorris@smu.edu>
2014-03-03 20:10:35 -0600

    Some change

--- user.php
+++ user.php
@@ -1,12 +1,9 @@
-<?php
 <!doctype html>
 <html>
 <head>
 	<title>Taco Truck -- Home</title>
 	<link href="css/styles.css" rel="stylesheet">
 </head>
-
-
 
 <body>
 	<div id="topBar"> <img src="img/taco_truck_logo.png" id="logo">
@@ -24,4 +21,3 @@
   <div style="text-align:center">Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
 </footer>
 </html>
-?>
