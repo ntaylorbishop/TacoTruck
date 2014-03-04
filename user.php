3474c77ebd90d0ff10d17dd4e6b9ea8b72a1aa20
CaptainMustang8 <nmorris@smu.edu>
2014-03-03 19:02:18 -0600

    Revision

--- user.php
+++ user.php
@@ -1,3 +1,4 @@
+<?php
 <!doctype html>
 <html>
 <head>
@@ -23,3 +24,4 @@
   <div style="text-align:center">Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
 </footer>
 </html>
+?>
