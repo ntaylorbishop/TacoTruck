50795fd8c40ffc2145711210e9e0c675ec9ffc2e
CaptainMustang8 <nmorris@smu.edu>
2014-03-03 21:05:03 -0600

    Added in form information

--- payment.php
+++ payment.php
@@ -16,6 +16,34 @@
 <div id="links"> <a href="#homepage">Home</a> | <a href="#account">Account</a> | <a href="#cart">Cart</a> | <a href="#logoff">Log Off</a>
     <div id="divider"></div>
   </div>
+  	<form name="payment" action="user.html" method="post">
+	  <div>First Name:
+	    <input type="text" id="fName" name="fName"/>
+	  </div>
+	  <div>Last Name:
+	    <input type="text" id="lName" name="lName"/>
+	  </div>
+	  <div>Email:
+	    <input type="email" id="email" name="email"/>
+	  </div>
+	  <div>Password:
+	    <input type="password" id="pwd" name="pwd"/>
+	  </div>
+	  <div>Confirm Password:
+	    <input type="password" id="pwdC" name="pwdC"/>
+	  </div>
+	  <div>Card Provider:
+	    <select id="ccType" name="ccType">
+	      <option value="AE">American Express</option>
+	      <option value="MC">MasterCard</option>
+	      <option value="V">Visa</option>
+	    </select>
+	  </div>
+	  <div>Card Number:
+	    <input type="text" id="ccNum" name="ccNum"/>
+	  </div>
+	  <input type="submit" id="register" name="register" value="Sign Up"/>
+	</form>
 </div>
 
 <h1 style="text-align:center">Taco Payment</h1>
