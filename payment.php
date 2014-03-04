82990095807ee24d4202ce07ebf85d7c72fe330b
CaptainMustang8 <nmorris@smu.edu>
2014-03-03 19:45:16 -0600

    Amended the title of the page

--- payment.php
+++ payment.php
@@ -12,6 +12,9 @@
   </div>
 </div>
 
+<h1>Taco Payment</h1>
+
+
 </body>
 <footer>
   <div style="text-align:center">Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
