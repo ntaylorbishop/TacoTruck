97078a203026dbee857415461c63081e31dc3f41
CaptainMustang8 <nmorris@smu.edu>
2014-03-03 19:40:26 -0600

    Added in the nav bar at the top and the footer

--- payment.php
+++ payment.php
@@ -5,4 +5,14 @@
 	<link href="css/styles.css" rel="stylesheet">
 </head>
 
+<body>
+	<div id="topBar"> <img src="img/taco_truck_logo.png" id="logo">
+    <div id="links"> <a href="#homepage">Home</a> 
+    <div id="divider"></div>
+  	</div>
+
+</body>
+<footer>
+  <div style="text-align:center">Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
+</footer>
 </html>
