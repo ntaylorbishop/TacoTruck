395aec5a8f12d1b59a21aa7d26217c4fd8ede99d
CaptainMustang8 <nmorris@smu.edu>
2014-03-04 18:54:10 -0600

    Deleted html styling

--- confirmation.php
+++ confirmation.php
@@ -17,7 +17,7 @@
     <div id="divider"></div>
   	</div>
 
-  	<h1 style="text-align:center">Order Confirmation</h1>
+  	<h1>Order Confirmation</h1>
   	<div id="confirm">
   		<h2>Your order is confirmed!</h2>
   		<p>Your order will be ready to pick up in 5-10 minutes.  Please pick it up at your chosen Taco Truck location.</p>
@@ -34,7 +34,7 @@
   		<h1>Thank you!  Come back soon!</h1>
   	</div>
 <footer>
-  <div style="text-align:center">Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
+  <div>Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
 </footer>
 </body>
 </html>
