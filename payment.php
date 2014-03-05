dd8292322e3bfce37de71503d6d720e1c30936fe
jkpolitz <jpolitz@smu.edu>
2014-03-05 12:13:07 -0600

    final commit before linking api to php front end

--- payment.php
+++ payment.php
@@ -3,14 +3,15 @@
 	for their tacos.-->
 <html>
 <head>
-	<title>Taco Truck -- Payment</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link href="css/styles.css" rel="stylesheet">
-	<link href="css/jquery-ui-1.10.4.custom.css" rel="stylesheet" />
-	<script src="js/jQuery.js"></script>
-	<script src="js/jquery-ui-1.10.4.custom.js"></script>
-	<script src="js/jquery-ui-1.10.4.custom.min.js"></script>
-	<script src="js/main.js"></script>
+<title>Taco Truck -- Payment</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/styles.css" rel="stylesheet">
+<link href="css/styles_payment.css" rel="stylesheet">
+<link href="css/jquery-ui-1.10.4.custom.css" rel="stylesheet" />
+<script src="js/jQuery.js"></script>
+<script src="js/jquery-ui-1.10.4.custom.js"></script>
+<script src="js/jquery-ui-1.10.4.custom.min.js"></script>
+<script src="js/main.js"></script>
 </head>
 
 <body>
@@ -18,70 +19,67 @@
   <div id="links"> <a href="#homepage">Home</a> &#x2022; <a href="#account">Account</a> &#x2022; <a href="#logoff">Log In</a></div>
   <div class="divider"></div>
 </div>
-<h1 id="title">Create an Account</h1>
- <h1>Taco Payment</h1>
- <div id="content">
-  	<form name="payment" action="user.html" method="post">
-  		<h2>Payment Information</h2>
-	  <div>First Name:
-	    <input type="text" id="fName" name="fName"/>
-	  </div>
-	  <div>Last Name:
-	    <input type="text" id="lName" name="lName"/>
-	  </div>
-	  <div>Email:
-	    <input type="email" id="email" name="email"/>
-	  </div>
-	  <div>Password:
-	    <input type="password" id="pwd" name="pwd"/>
-	  </div>
-	  <div>Confirm Password:
-	    <input type="password" id="pwdC" name="pwdC"/>
-	  </div>
-	  <div>Card Provider:
-	    <select id="ccType" name="ccType">
-	      <option value="AE">American Express</option>
-	      <option value="MC">MasterCard</option>
-	      <option value="V">Visa</option>
-	    </select>
-	  </div>
-	  <div>Card Number:
-	    <input type="text" id="ccNum" name="ccNum"/>
-	  </div>
-	  <!--<input type="submit" id="register" name="register" value="Sign Up"/>-->
-	</form>
+<h1 id="title">Taco Payment</h1>
+<div id="content">
+  <div id="payment">
+    <form name="payment" action="user.html" method="post">
+      <h2>Payment Information</h2>
+      <div><label>First Name:</label>
+        <input type="text" id="fName" name="fName"/>
+      </div>
+      <div><label>Last Name:</label>
+        <input type="text" id="lName" name="lName"/>
+      </div>
+      <div><label>Email:</label>
+        <input type="email" id="email" name="email"/>
+      </div>
+      <div><label>Password:</label>
+        <input type="password" id="pwd" name="pwd"/>
+      </div>
+      <div><label>Confirm Password:</label>
+        <input type="password" id="pwdC" name="pwdC"/>
+      </div>
+      <div><label>Card Provider:</label>
+        <select id="ccType" name="ccType">
+          <option value="AE">American Express</option>
+          <option value="MC">MasterCard</option>
+          <option value="V">Visa</option>
+        </select>
+      </div>
+      <div><label>Card Number:</label>
+        <input type="text" id="ccNum" name="ccNum"/>
+      </div>
+      <!--<input type="submit" id="register" name="register" value="Sign Up"/>-->
+    </form>
+  </div>
+  <div id="order">
+    <h2>Your Order</h2>
+    <table>
+      <tr>
+        <th>Quantity</th>
+        <th>Taco</th>
+        <th>Price</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Beef Fajita</td>
+        <td>3.75</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Brushfire</td>
+        <td>4.15</td>
+      </tr>
+    </table>
+    <h3>Order Total: </h3>
+    <form name="checkout" action="confirmation.php" method="post">
+      <input type="submit" id="checkout" name="checkout" value="Check Out"/>
+    </form>
+  </div>
 </div>
-</div>
-
-<div>
-	<h2>Your Order</h2>
-	<table>
-		<tr>
-			<th>Quantity</th>
-			<th>Taco</th>
-			<th>Price</th>
-		</tr>
-		<tr>
-			<td>1</td>
-			<td>Beef Fajita</td>
-			<td>3.75</td>
-		</tr>
-		<tr>
-			<td>1</td>
-			<td>Brushfire</td>
-			<td>4.15</td>
-		</tr>
-	</table>
-	<h3>Order Total:  </h3>
-	<form name="checkout" action="confirmation.php" method="post">
-		<input type="submit" id="checkout" name="checkout" value="Check Out"/>
-	</form>
-</div>
-
-<footer>
+<div class="divider2"></div>
+<div id="footer">
   <div>Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
-</footer>
+</div>
 </body>
-
-
 </html>
