dedd79f87dbadea2f07e4cf9b483aef979b1c434
jkpolitz <jpolitz@smu.edu>
2014-03-06 12:58:32 -0800

    fixed footer on buildTaco.php

--- buildTaco.php
+++ buildTaco.php
@@ -24,72 +24,6 @@
 <div id="ingredients">
   <form id="currentTaco">
     <div id="accordion">
-      <h3>Filling</h3>
-      <div>
-        <input type="radio" name="filling" value="steak" />
-        Steak<br />
-        <input type="radio" name="filling" value="chicken" />
-        Chicken<br />
-        <input type="radio" name="filling" value="carnitas" />
-        Carnitas<br />
-        <input type="radio" name="filling" value="veggies" />
-        Veggies<br />
-      </div>
-      <h3>Tortilla</h3>
-      <div>
-        <input type="radio" name="tortilla" value="flour" />
-        Flour<br />
-        <input type="radio" name="tortilla" value="cayenne" />
-        Cayenne<br />
-        <input type="radio" name="tortilla" value="wheat" />
-        Wheat<br />
-        <input type="radio" name="tortilla" value="spinach" />
-        Spinach<br />
-      </div>
-      <h3>Rice</h3>
-      <div>
-        <input type="radio" name="rice" value="cilantro" />
-        Cilantro Rice<br />
-        <input type="radio" name="rice" value="spanish" />
-        Spanish Rice<br />
-      </div>
-      <h3>Cheese</h3>
-      <div>
-        <input type="radio" name="cheese" value="quesoFresco" />
-        Queso Fresco<br />
-        <input type="radio" name="cheese" value="cheddar/jack" />
-        Cheddar/Jack Mix<br />
-        <input type="radio" name="cheese" value="monterreyJack" />
-        Monterrey Jack<br />
-      <div/>
-      <h3>Beans</h3>
-      <div>
-        <input type="radio" name="beans" value="refried" />
-        Refried Beans<br />
-        <input type="radio" name="beans" value="pinto" />
-        Whole Pinto Beans<br />
-        <input type="radio" name="beans" value="black" />
-        Black Beans<br />
-      <div/>
-      <h3>Sauce</h3>
-      <div>
-      <h3>Vegetables</h3>
-      <div>
-        <input type="checkbox" name="vegetables" value="lettuce"/>
-        Lettuce<br />
-        <input type="checkbox" name="vegetables" value="tomatoes"/>
-        Tomatoes<br />
-        <input type="checkbox" name="vegetables" value="peppers"/>
-      </div>
-      <h3>Extras</h3>
-      <div>
-        <input type="checkbox" name="extras" value="guac" />
-        Guac<br />
-        <input type="checkbox" name="extras" value="sourCream" />
-        Sour Cream<br />
-        <input type="checkbox" name="extras" value="queso" />
-        Queso<br />
-      </div>
     </div>
     <input type="number" name="quantity" min="1" />
     Quantity:
@@ -105,8 +39,9 @@
   </form>
   </div>
 </div>
-<footer>
+<div class="divider2"></div>
+<div id="footer">
   <div style="text-align:center">Taco Truck |<a href="#locations">123 Taco Avenue - Dallas - TX - 75205</a>| 214-MY-TACOS (214-698-2267) |<a href="#homepage">tacotruck.com</a>| Hours: 24/7</div>
-</footer>
+</div>
 </body>
 </html>
